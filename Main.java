64d0ac5bed82fb5cf924f8f3055c803e20ad16a1
Niam Pattni <niam.pattni@gmail.com>
2016-05-11 15:22:48 +0000

    C9 test

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@
 public class Main {//test
   // Main Method
   public static void main(String[] args) {
-    
+    //do you wanna build a snowman?
+    for (int x=0;x<10;x++)
+    {
+      System.out.println("YES I DO WANT TO BUILD A SNOWMAN!")
+    } 
+    }
   }
 }
