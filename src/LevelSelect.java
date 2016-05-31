5ab0119759c695ea268a6dbb34dadf482c8c6196
niampattni <niam.pattni@gmail.com>
2016-05-31 10:25:03 -0400

    Version 4
    May 31, 2016 - 10:24am

---
+++ src/LevelSelect.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+public class LevelSelect extends JPanel {
+  
+  BufferedImage bImg;
+  
+  public LevelSelect() {
+    JLabel bg;
+    try {
+      bImg = ImageIO.read(this.getClass().getResource("TestBG.png"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    bg = new JLabel(new ImageIcon(bImg));
+    add(bg);
+  }
+  
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.drawImage(bImg, 0, 0, this);
+  }
+  
+}
