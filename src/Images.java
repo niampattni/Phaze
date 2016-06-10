dd26d345b612e709e6d4f24d2b2d2a622988a925
niampattni <niam.pattni@gmail.com>
2016-06-10 02:16:40 -0400

    so close at this point
    why can't it just be overrrrrrr

--- src/Images.java
+++ src/Images.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 public class Images {
-  public static String[] names = {"TeddyBear", "CandyCane", "Cake", "Balloons", "Tree", "Elephant", "Flower", "Hearts", "Snail", "Duck"};
-  public static int[] completion = {20, 18, 15, 7, 6, 17, 5, 9, 6, 7};
+  public static String[] names = {"TeddyBear", "CandyCane", "Cake", "Balloons", "Tree", "Elephant", "Flower", "Hearts", "Snail", "Duck", "Turkey", "Star"};
+  public static int[] completion = {20, 18, 15, 7, 6, 17, 5, 9, 6, 7, 24, 7};
   public static int level = 0;
   public static int current = -1;
   public static JPanel panel = new JPanel();
